1b2e0f65ea2302f2d72a75f8205812a9ac18a6a7
Teslospisi <you@example.com>
2024-05-10 12:53:24 +0200

    🐣

---
+++ ratio.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+
+int main() {
+
+// Declaration and initilization of variables
+double height = 10;
+double width = 20;
+double ratio;
+// Definition of ratio:
+ratio = height / width;
+std::cout << "The ratio is "<<ratio<<".\n";
+
+}
